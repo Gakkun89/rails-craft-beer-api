96aa3597293c6ddb7575bc3fa646fb0480dc3838
Mike Warren <mikeywarren89@gmail.com>
2020-04-03 11:12:50 +0900

    added seeds to load in the beers from json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+path = './beers.json'
+
+beers = JSON.parse(File.read(path))
+
+beers.each do |beer|
+  Beer.create!(
+    name: beer[1]['name'],
+    brewery: beer[1]['brewery'],
+    style: beer[1]['style'],
+    abv: beer[1]['abv'],
+    desc: beer[1]['desc'],
+    ibu: beer[1]['ibu'].to_i,
+    rating: beer[1]['rating'].to_f,
+    image: 'TODO'
+  )
+end
