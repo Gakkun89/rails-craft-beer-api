c2612511923ece93c51dde3446c70b417d611d05
Mike Warren <mikeywarren89@gmail.com>
2020-04-03 11:31:19 +0900

    added search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :beers, only: [:index, :show]
+      get '/search', to: 'beers#search', as: 'search'
     end
   end
 end
