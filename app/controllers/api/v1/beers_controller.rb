3bd010afd1df06fb02d7ae30e4a23eb1db749931
Mike Warren <mikeywarren89@gmail.com>
2020-04-03 09:54:36 +0900

    added skeleton to beers controller

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,2 +1,19 @@
-class BeersController < ApplicationController
+class Api::V1::BeersController < ApplicationController
+  before_action :set_beer, only: :show
+
+  def index
+    @beers = Beer.all
+
+    render json: @beers
+  end
+
+  def show
+    render json: @beer
+  end
+
+  private
+
+  def set_beer
+    @beer = Beer.find(params[:id])
+  end
 end
