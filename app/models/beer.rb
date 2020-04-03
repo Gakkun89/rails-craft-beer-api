be565bdf8180debc594b7ad3a8d4d7b745232d3d
Mike Warren <mikeywarren89@gmail.com>
2020-04-03 11:26:50 +0900

    added pgsearch info to beers model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,10 @@
 class Beer < ApplicationRecord
+  include PgSearch
+  pg_search_scope :search_by_term, against: [:name, :brewery, :style, :desc],
+    using: {
+      tsearch: {
+        any_word: true,
+        prefix: true
+      }
+    }
 end
